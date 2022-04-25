ae742fb7b33b34661eb2d5d25f7944f2f00f4c26
CoderFaruk <66289933+CodersFaruk@users.noreply.github.com>
2022-04-25 15:54:45 +0600

    admin side bar complete

--- Simple-laravel-project/resources/views/admin/admin.blade.php
+++ Simple-laravel-project/resources/views/admin/admin.blade.php
@@ -1,23 +1,35 @@
 @extends('admin.layout.app')
 
 @section('admin_content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Admin Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in! Admin Panel') }}
-                </div>
-            </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-2">
+            <ul class="nav flex-column sidebar">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Add Product</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Product List</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link ">More Details...</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link ">More Details...</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link ">More Details...</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link ">setting</a>
+                </li>
+              </ul>
         </div>
+        <div class="col-md-10"></div>
+        
     </div>
 </div>
 @endsection
